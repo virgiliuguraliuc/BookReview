20c66beb95fe365ed5d89aab1c9adf2d82d89335
virgiliu <virgiliuguraliuc2@gmail.com>
2019-09-18 00:02:45 +0300

    Adding base

---
+++ js/login.js
@@ -0,0 +1,26 @@
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if ( username == "Formget" && password == "formget#123"){
+        alert ("Login successfully");
+        //check on the server nu pe html cu hashes
+        window.location = "success.html"; // Redirecting to other page.
+        return false;
+    }
+    else{
+        attempt --;// Decrementing by one.
+        alert("You have left "+attempt+" attempt;");
+// Disabling fields after 3 attempts.
+        //asta ar trebui sa mearga spre server si serveru sa enable login timer
+        if( attempt == 0){
+            document.getElementById("username").disabled = true;
+            document.getElementById("password").disabled = true;
+            document.getElementById("submit").disabled = true;
+            return false;
+        }
+    }
+}
+
+ 
